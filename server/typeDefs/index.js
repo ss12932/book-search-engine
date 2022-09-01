e68af0eb5dd05e31edc72bdfc7943bba6fc92680
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-09-01 13:30:27 +0100

    defined typedefs

---
+++ server/typeDefs/index.js
@@ -0,0 +1,61 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Book {
+    bookId: ID!
+    authors: [String!]
+    description: String!
+    image: String
+    link: String
+    title: string!
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    password: String
+    savedBooks: [Book]
+    bookCount: Int!
+  }
+
+  type SignupSuccess {
+    success: Boolean!
+  }
+  type LoginSuccess {
+    success: Boolean!
+    token: String!
+    user: User!
+  }
+
+  input SignupInput {
+    username: String!
+    email: String!
+    password: String!
+  }
+
+  input LoginInput {
+    email: String!
+    password: String!
+  }
+
+  input SaveBookInput {
+    bookId: String!
+    authors: [String]
+    image: String
+    title: String
+    description: String
+    link: String
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    signup(input: SignupInput!): SignupSuccess
+    login(input: LoginInput!): LoginSuccess
+    saveBook(input: SaveBookInput!): User!
+    removeBook(bookId: String!): User!
+  }
+`;
