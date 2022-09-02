a685b139607c34b66f8ae4188d77d3462f4f0004
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-09-02 16:32:11 +0100

    remove redundant api import

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
 
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 import { LOGIN } from '../graphql/mutations';
