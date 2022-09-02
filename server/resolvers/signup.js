abccfb3dddcab79c05695ed7d26fb09b290a2f09
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-09-02 16:54:16 +0100

    update incorrect imports for auth

--- server/resolvers/signup.js
+++ server/resolvers/signup.js
@@ -1,7 +1,7 @@
 const { ApolloError } = require('apollo-server');
 
 const { User } = require('../models');
-const { signToken } = require('../utils/auth');
+const { signToken } = require('../context/auth');
 
 const signup = async (_, { input }) => {
   const user = await User.create(input);
